af9be63472e4e061bb27ad2e240fbae6a4a6a726
Lungu Marian <lungumarian15@gmail.com>
2025-03-13 11:10:52 +0200

    myCv

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+
+// Import our custom CSS
+import './scss/styles.scss';
+
+import '../node_modules/typed.js/dist/typed.umd.js'
+
+import './js/main.js';
